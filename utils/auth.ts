6269cdb20ab51657875f7b2322c0dbea95d39abe
Diana Didorenko <125818555+dianadidorenko@users.noreply.github.com>
2024-01-15 20:37:47 +0200

    authOptions fix 8

--- utils/auth.ts
+++ utils/auth.ts
@@ -4,7 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/db";
 
-const authOptions: AuthOptions = {
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
