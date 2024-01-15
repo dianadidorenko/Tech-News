a62e4400b62e66f297eb0d08cf63fbd5deccf596
Diana Didorenko <125818555+dianadidorenko@users.noreply.github.com>
2024-01-15 21:02:05 +0200

    authOptions fix 14

--- components/Post.tsx
+++ components/Post.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import DeleteButton from "./DeleteButton";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import authOptions from "@/utils/options";
 
 interface PostProps {
   id: string;
